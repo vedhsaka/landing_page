eee9b76b522634d62fd7850863a8a45d06cea57b
Akash Thakur <akashthakur@Akashs-Air.attlocal.net>
2024-07-23 22:27:22 -0700

    Change url

--- app/api/robots/route.ts
+++ app/api/robots/route.ts
@@ -4,7 +4,7 @@ export async function GET() {
   return new NextResponse(`User-agent: *
 Allow: /
 
-Sitemap: https://withflame.com/sitemap.xml`, {
+Sitemap: https://NotHotDog.dev/sitemap.xml`, {
     headers: { 'Content-Type': 'text/plain' },
   })
 }
