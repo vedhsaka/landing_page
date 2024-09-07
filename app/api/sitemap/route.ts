8e81c3d2779ca640058738ffa2a99a88f85b2566
vedhsaka <akashkthakur@icloud.com>
2024-09-07 04:26:38 -0700

    Remove blogs

--- app/api/sitemap/route.ts
+++ app/api/sitemap/route.ts
@@ -9,44 +9,7 @@ export async function GET() {
     <changefreq>daily</changefreq>
     <priority>1.0</priority>
   </url>
-  <url>
-    <loc>https://nothotdog.dev/blog/</loc>
-    <lastmod>2024-07-19</lastmod>
-    <changefreq>daily</changefreq>
-    <priority>1.0</priority>
-  </url>
-  <url>
-    <loc>https://nothotdog.dev/blog/voice-ai-in-restaurant-drive-throughs</loc>
-    <lastmod>2024-07-06</lastmod>
-    <changefreq>weekly</changefreq>
-    <priority>0.8</priority>
-  </url>
-  <url>
-    <loc>https://nothotdog.dev/blog/voice-ai-evolution</loc>
-    <lastmod>2024-07-06</lastmod>
-    <changefreq>weekly</changefreq>
-    <priority>0.8</priority>
-  </url>
-
-  <url>
-    <loc>https://nothotdog.dev/blog/voice-ai-customer-care</loc>
-    <lastmod>2024-07-06</lastmod>
-    <changefreq>weekly</changefreq>
-    <priority>0.8</priority>
-  </url>
-
-  <url>
-    <loc>https://nothotdog.dev/blog/character-ai</loc>
-    <lastmod>2024-07-09</lastmod>
-    <changefreq>weekly</changefreq>
-    <priority>0.8</priority>
-  </url>
-  <url>
-  <loc>https://nothotdog.dev/blog/voice-ai-healthcare</loc>
-  <lastmod>2024-07-19</lastmod>
-  <changefreq>weekly</changefreq>
-  <priority>0.8</priority>
-</url>
+  
 </urlset>`, {
     headers: { 'Content-Type': 'application/xml' },
   })
--- app/page.tsx
+++ app/page.tsx
@@ -138,11 +138,6 @@ export default function HomeContent() {
     {/* Footer */}
     <footer className="bg-black text-white text-center p-4 border-t border-gray-600 mt-4">
       <p>&copy; NotHotDog 2024</p>
-      <p>
-        <a href="/blog" className="text-white underline">
-          Visit our Blog
-        </a>
-      </p>
     </footer>
 
     </div>
